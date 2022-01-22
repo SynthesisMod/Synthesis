1a3ff1da426e2f2d09c9bd30bb7c938742bd3d41
Desco <63560727+Desco1@users.noreply.github.com>
2022-01-22 10:49:30 +0100

    oh lawd it's comin' (0.1.0)

--- src/main/java/com/luna/synthesis/Synthesis.java
+++ src/main/java/com/luna/synthesis/Synthesis.java
@@ -26,7 +26,7 @@ import java.io.File;
 public class Synthesis {
     public static final String NAME = "Synthesis";
     public static final String MODID = "synthesis";
-    public static final String VERSION = "Alpha-v4";
+    public static final String VERSION = "0.1.0";
     public static final String configLocation = "./config/synthesis.toml";
 
     @Getter private static Synthesis instance;
