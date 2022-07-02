0a84ad0665655bcf97cd159aaaa35ec54f13e46a
RayDeeUx <raydeeux@gmail.com>
2022-07-02 13:58:22 -0400

    i did a thing and i forgot what it was

--- src/main/java/com/luna/synthesis/features/utilities/FindSomeonesSkyblockInfo.java
+++ src/main/java/com/luna/synthesis/features/utilities/FindSomeonesSkyblockInfo.java
@@ -6,6 +6,7 @@ import com.luna.synthesis.events.MessageSentEvent;
 import com.luna.synthesis.utils.ChatLib;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.EnumChatFormatting;
 
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -29,9 +30,10 @@ public class FindSomeonesSkyblockInfo {
         String nameToCheck = "";
         if (event.message.endsWith(config.utilitiesShareText) || event.message.endsWith(config.utilitiesShareBootsText) || event.message.endsWith(config.utilitiesShareHelmetText) || event.message.endsWith(config.utilitiesShareLeggingsText) || event.message.endsWith(config.utilitiesShareChestplateText)) {return;}
         if (!config.utilitiesCheckWeight && event.message.startsWith("[weight")) {event.setCanceled(true); ChatLib.chat("You have the setting disabled. Please enable it and try again, but do so with extreme caution.");return;}
+        if (event.message.startsWith("[stats")) {checkSomeonesStats(event.message); event.setCanceled(true); return;}
         if (event.message.startsWith("[weight")) {
             if (event.message.endsWith("[weight]")) {
-                nameToCheck = Minecraft.getMinecraft().thePlayer.getName().toLowerCase();
+                nameToCheck = Minecraft.getMinecraft().thePlayer.getName();
             } else {
                 nameToCheck = (event.message.toLowerCase().replace("[weight ", "").replace("]", "")).replace(" ", "");
             }
@@ -63,10 +65,12 @@ public class FindSomeonesSkyblockInfo {
                 JsonObject profiles = data.get("profiles").getAsJsonObject();
                 Set<Map.Entry<String, JsonElement>> profileSet = profiles.entrySet();
                 JsonElement currentSbProfileWeightData = null;
+                String displayName = "";
 
                 for (Map.Entry<String,JsonElement> me : profileSet)
                 {
                     if (me.getValue().getAsJsonObject().get("current").getAsBoolean()) {
+                        displayName = ((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("display_name").getAsString();
                         currentSbProfileWeightData = ((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("weight");
                     }
                 }
@@ -74,7 +78,7 @@ public class FindSomeonesSkyblockInfo {
                 int overallSenitherWeight = ((int)((JsonObject)(currentSbProfileWeightData).getAsJsonObject().get("senither")).get("overall").getAsDouble());
                 int overallLilyWeight = ((int)((JsonObject)(currentSbProfileWeightData).getAsJsonObject().get("lily")).get("total").getAsDouble());
 
-                ChatLib.chat(nameToCheck + "'s Senither weight to the nearest integer is " + overallSenitherWeight + " and their Lily weight to the nearest integer is " + overallLilyWeight + ".");
+                ChatLib.chat(displayName + "'s Senither weight to the nearest integer is " + overallSenitherWeight + " and their Lily weight to the nearest integer is " + overallLilyWeight + ".");
                 
             } catch (Exception e) {
                 ChatLib.chat("Synthesis ran into a problem checking content from SkyCrypt. See logs.");
@@ -84,6 +88,71 @@ public class FindSomeonesSkyblockInfo {
         } catch (Exception e) {
             ChatLib.chat("Synthesis ran into a problem checking " + nameToCheck + "'s weight. See logs.");
             System.out.println("Synthesis ran into a problem checking " + nameToCheck + "'s weight. See logs.");
+            e.printStackTrace();
+        }
+    }
+
+    public void checkSomeonesStats(String thatOneParameter) {
+        String theNameToCheck = "";
+        if (thatOneParameter.endsWith(config.utilitiesShareText) || thatOneParameter.endsWith(config.utilitiesShareBootsText) || thatOneParameter.endsWith(config.utilitiesShareHelmetText) || thatOneParameter.endsWith(config.utilitiesShareLeggingsText) || thatOneParameter.endsWith(config.utilitiesShareChestplateText) || thatOneParameter.startsWith("[weight")) {return;}
+        if (thatOneParameter.startsWith("[stats")) {
+            if (thatOneParameter.endsWith("[stats]")) {
+                theNameToCheck = Minecraft.getMinecraft().thePlayer.getName();
+            } else {
+                theNameToCheck = (thatOneParameter.toLowerCase().replace("[stats ", "").replace("]", "")).replace(" ", "");
+            }
+        }
+        try {
+            URL url = new URL("https://sky.shiiyu.moe/api/v2/profile/" + theNameToCheck);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setDoOutput(true);
+            http.setDoInput(true);
+            http.setRequestProperty("User-Agent", "SynthesisMod-NONCANON");
+            http.setRequestProperty("Accept", "application/json");
+            http.setRequestProperty("Method", "GET");
+            http.connect();
+            try (InputStream instream = http.getInputStream()) {
+                if (http.getResponseCode() != 200) {
+                    ChatLib.chat("Synthesis did not get an A-OK response from SkyCrypt. The response code Synthesis got instead was " + http.getResponseCode() + ". Aborting mission.");
+                    return;
+                }
+                JsonParser parser = new JsonParser();
+                JsonObject data = parser.parse(new String(IOUtils.toByteArray(instream), StandardCharsets.UTF_8)).getAsJsonObject();
+                if (!data.has("profiles")) {
+                    ChatLib.chat("Synthesis failed in getting information from SkyCrypt. Either their API is down or this player doesn't have any profiles at all. Aborting mission.");
+                    return;
+                }
+
+                JsonObject profiles = data.get("profiles").getAsJsonObject();
+                Set<Map.Entry<String, JsonElement>> profileSet = profiles.entrySet();
+                int currentSbProfileSkillAverage = 0;
+                int currentSbProfileSlayerXp = 0;
+                String displayName = "";
+                String firstJoinText = "";
+
+                for (Map.Entry<String,JsonElement> me : profileSet)
+                {
+                    if (me.getValue().getAsJsonObject().get("current").getAsBoolean()) {
+                        displayName = ((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("display_name").getAsString();
+                        currentSbProfileSkillAverage = ((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("average_level").getAsInt();
+                        currentSbProfileSlayerXp = ((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("slayer_xp").getAsInt();
+                        firstJoinText = (((JsonObject)((JsonObject)(me.getValue().getAsJsonObject().get("data"))).get("first_join")).get("text")).getAsString();
+                    }
+                }
+                
+                String skillAvg = (EnumChatFormatting.BLUE + "" + currentSbProfileSkillAverage + " skill average");
+                String slayerXP = (EnumChatFormatting.RED + "" + currentSbProfileSlayerXp + " Slayer XP");
+                firstJoinText = (EnumChatFormatting.GREEN + "First joined " + firstJoinText);
+
+                ChatLib.chat("Here are " + displayName + "'s stats:\n §7- " + skillAvg + "\n §7- " + slayerXP + "\n §7- " + firstJoinText);
+            } catch (Exception e) {
+                ChatLib.chat("Synthesis ran into a problem checking content from SkyCrypt. See logs.");
+                System.out.println("Synthesis ran into a problem checking content from SkyCrypt. See logs.");
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            ChatLib.chat("Synthesis ran into a problem checking " + theNameToCheck + "'s weight. See logs.");
+            System.out.println("Synthesis ran into a problem checking " + theNameToCheck + "'s weight. See logs.");
             e.printStackTrace();
         }
     }
