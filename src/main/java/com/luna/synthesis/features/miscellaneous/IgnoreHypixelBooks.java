94c1ed4e9f178108f30a26f812ca683f15c45c9a
RayDeeUx <raydeeux@gmail.com>
2022-07-13 21:40:29 -0400

    skyblock check

--- src/main/java/com/luna/synthesis/features/miscellaneous/IgnoreHypixelBooks.java
+++ src/main/java/com/luna/synthesis/features/miscellaneous/IgnoreHypixelBooks.java
@@ -5,7 +5,9 @@ import gg.essential.api.EssentialAPI;
 import com.luna.synthesis.Synthesis;
 import com.luna.synthesis.core.Config;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreenBook;
+import net.minecraft.util.StringUtils;
 
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,7 +18,7 @@ public class IgnoreHypixelBooks {
     
     @SubscribeEvent
     private void onBook(GuiOpenEvent e) {
-        if (config.miscIgnoreHypixelBooks && EssentialAPI.getMinecraftUtil().isHypixel() && e.gui instanceof GuiScreenBook) {
+        if ((config.miscIgnoreHypixelBooks) && (EssentialAPI.getMinecraftUtil().isHypixel()) && (e.gui instanceof GuiScreenBook) && (StringUtils.stripControlCodes(Minecraft.getMinecraft().theWorld.getScoreboard().getObjectiveInDisplaySlot(1).getDisplayName()).contains("SKYBLOCK"))) {
             e.setCanceled(true);
         }
     }
