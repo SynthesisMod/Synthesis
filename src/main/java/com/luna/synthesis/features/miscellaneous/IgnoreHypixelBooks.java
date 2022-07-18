bd33b6a53ebc29e4dd90c8c9a50780b840de7623
RayDeeUx <raydeeux@gmail.com>
2022-07-18 12:53:36 -0400

    ascynx FINAL

--- src/main/java/com/luna/synthesis/features/miscellaneous/IgnoreHypixelBooks.java
+++ src/main/java/com/luna/synthesis/features/miscellaneous/IgnoreHypixelBooks.java
@@ -5,6 +5,7 @@ import com.luna.synthesis.core.Config;
 import com.luna.synthesis.utils.ChatLib;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenBook;
 
 import net.minecraftforge.client.event.GuiScreenEvent;
@@ -17,7 +18,8 @@ public class IgnoreHypixelBooks {
     @SubscribeEvent
     private void onBook(GuiScreenEvent.KeyboardInputEvent e) {
         if (!config.miscIgnoreHypixelBooks) {return;}
-        if ((Minecraft.getMinecraft().currentScreen instanceof GuiScreenBook)) {
+        GuiScreen screen = Minecraft.getMinecraft().currentScreen;
+        if ((screen instanceof GuiScreenBook)) {
             //e.setCanceled(true);
             //Minecraft.getMinecraft().thePlayer.closeScreen();
             ChatLib.chat("Ascynx won't be able to see this message in-game because BRUH");
