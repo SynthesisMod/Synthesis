b4b288d2b534ac8ff0a77eab28046e326f6cd91a
RayDeeUx <raydeeux@gmail.com>
2022-07-20 11:47:49 -0400

    innit

--- src/main/java/com/luna/synthesis/features/utilities/MasterModeSoulDetector.java
+++ src/main/java/com/luna/synthesis/features/utilities/MasterModeSoulDetector.java
@@ -39,11 +39,7 @@ public class MasterModeSoulDetector {
             if (s.contains("Absorbed Souls")) {
                 findSouls(itemStack);
                 int anchor = tooltip.indexOf(s);
-                for (int h = 0; h < numSouls; h++) {
-                    if (whichOnesAreMasterSouls.get(h)) {
-                        tooltip.set(h+1+anchor, tooltip.get(h+1+anchor) + " §d(from Master Mode)");
-                    }
-                }
+                for (int h = 0; h < numSouls; h++) if (whichOnesAreMasterSouls.get(h)) tooltip.set(h + 1 + anchor, tooltip.get(h + 1 + anchor) + " §d(from Master Mode)");
             }
         }
     }
@@ -51,65 +47,37 @@ public class MasterModeSoulDetector {
     @SubscribeEvent
     public void guiRender(GuiScreenEvent.BackgroundDrawnEvent e) {
         if (!(Minecraft.getMinecraft().currentScreen instanceof GuiChest)) return;
-        if ((!(StringUtils.stripControlCodes(
-                ((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer))
-                .getLowerChestInventory().getDisplayName().getUnformattedText())
-                .contains("You")
-            )) &&
-            (!(StringUtils.stripControlCodes(
-                ((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer))
-                .getLowerChestInventory().getDisplayName().getUnformattedText())
-                .contains("Auctions:")
-            )) &&
-            (!(StringUtils.stripControlCodes(
-                ((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer))
-                .getLowerChestInventory().getDisplayName().getUnformattedText())
-                .contains("Auction View")
-            ))
-        ) return;
+        if ((!(StringUtils.stripControlCodes(((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer)).getLowerChestInventory().getDisplayName().getUnformattedText()).contains("You"))) && (!(StringUtils.stripControlCodes(((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer)).getLowerChestInventory().getDisplayName().getUnformattedText()).contains("Auctions:"))) && (!(StringUtils.stripControlCodes(((ContainerChest)(Minecraft.getMinecraft().thePlayer.openContainer)).getLowerChestInventory().getDisplayName().getUnformattedText()).contains("Auction View")))) return;
         int numTrues = 0;
         List<Slot> slots = ((GuiChest)(e.gui)).inventorySlots.inventorySlots;
         for (Slot s : slots) {
             if (s.getStack() != null) {
                 findSouls(s.getStack());
-                for (boolean bool : whichOnesAreMasterSouls) {
-                    if (bool) {
-                        numTrues++;
+                for (boolean bool : whichOnesAreMasterSouls) if (bool) numTrues++;
+                if (!(whichOnesAreMasterSouls.isEmpty())) {
+                    if ((numTrues == whichOnesAreMasterSouls.size())) {
+                        r = b = 0;
+                        g = 255;
+                    } else {
+                        r = g = 255;
+                        b = 0;
                     }
-                }
-                if ((numTrues == whichOnesAreMasterSouls.size()) && (0 != whichOnesAreMasterSouls.size())) {
-                    r = b = 0; g = 255;
-                } else {
-                    r = 0; g = b = 128;
+                    Color bgColor = new Color(r, g, b);
+                    GL11.glTranslated(0, 0, 1);
+                    Gui.drawRect(((((new ScaledResolution(Minecraft.getMinecraft())).getScaledWidth() - 176) / 2) + s.xDisplayPosition), ((slots.size() != 90) ? (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition) + ((6 - (slots.size() - 36) / 9) * 9)) : ((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition)), (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledWidth() - 176) / 2) + s.xDisplayPosition) + 16), (((slots.size() != 90) ? (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition) + ((6 - (slots.size() - 36) / 9) * 9)) : ((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition)) + 16),bgColor.getRGB());
+                    GL11.glTranslated(0, 0, -1);
                 }
             }
-            Color bgColor = new Color(r, g, b);
-            GL11.glTranslated(0, 0, 1);
-            Gui.drawRect(
-                ((((new ScaledResolution(Minecraft.getMinecraft())).getScaledWidth() - 176) / 2) + s.xDisplayPosition),
-                ((slots.size() != 90) ?
-                    (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition) +
-                        ((6 - (slots.size() - 36) / 9) * 9)) :
-                    ((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition)),
-                (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledWidth() - 176) / 2) + s.xDisplayPosition) + 16),
-                (((slots.size() != 90) ?
-                    (((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition) +
-                        ((6 - (slots.size() - 36) / 9) * 9)) :
-                    ((((new ScaledResolution(Minecraft.getMinecraft())).getScaledHeight() - 222) / 2) + s.yDisplayPosition)) + 16),
-                bgColor.getRGB());
-            GL11.glTranslated(0, 0, -1);
         }
     }
 
     private void findSouls(ItemStack is) {
+        if (!(whichOnesAreMasterSouls.isEmpty())) whichOnesAreMasterSouls.clear();
         if (!(is.hasTagCompound())) return;
         if (is.getSubCompound("ExtraAttributes", false) == null || (!(is.getSubCompound("ExtraAttributes", false).hasKey("necromancer_souls")))) return;
         NBTTagList tl = ((NBTTagList)((is.getSubCompound("ExtraAttributes", false)).getTag("necromancer_souls")));
         if (tl.tagCount() == 0) return;
-        if (!(whichOnesAreMasterSouls.isEmpty())) whichOnesAreMasterSouls.clear();
         numSouls = tl.tagCount();
-        for (int i = 0; i < numSouls; i++) {
-            whichOnesAreMasterSouls.add(tl.get(i).toString().contains("MASTER_"));
-        }
+        for (int i = 0; i < numSouls; i++) whichOnesAreMasterSouls.add(tl.get(i).toString().contains("MASTER_"));
     }
 }
