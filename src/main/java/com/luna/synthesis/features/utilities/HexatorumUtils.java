427667f3fdf0d5f72b4fbc53247cbb691a7317bb
RayDeeUx <raydeeux@gmail.com>
2022-07-27 12:25:21 -0400

    cant test rn but let's pray this works

--- src/main/java/com/luna/synthesis/features/utilities/HexatorumUtils.java
+++ src/main/java/com/luna/synthesis/features/utilities/HexatorumUtils.java
@@ -27,12 +27,12 @@ import org.lwjgl.opengl.GL11;
 public class HexatorumUtils {
 
     private final Config config = Synthesis.getInstance().getConfig();
-    private EntityPlayerSP mgmtp = Minecraft.getMinecraft().thePlayer;
+    private EntityPlayerSP mcgmtp = Minecraft.getMinecraft().thePlayer;
     private List<String> itemLore = new ArrayList<String>();
     private ArrayList<Float> floats = new ArrayList<Float>();
     private List<Slot> slots;
     private String menuName = "";
-    private float r = 0F, g = 0F, b = 0F;
+    private float r = 0F, g = 0F, b = 0F, floatForArrayList = 0F;
     
     @SubscribeEvent
     public void onHexUI(GuiScreenEvent.BackgroundDrawnEvent e) {
@@ -48,44 +48,27 @@ public class HexatorumUtils {
                 if (!(itemLore.isEmpty())) itemLore.clear();
             } catch (Exception ex) {}
             if (!(floats.isEmpty())) floats.clear();
+            floatForArrayList = 0F;
             if (sl.getStack() == null || !(sl.getStack().hasDisplayName())) continue;
             ItemStack is = sl.getStack();
             String dName = is.getDisplayName();
             if (dName.contains("aEnchantments")) {
-                itemLore = is.getTooltip(mgmtp, false);
-                int currEnch, maxEnch = 0;
+                itemLore = is.getTooltip(mcgmtp, false);
+                float currEnch, maxEnch = 0;
                 colorThisSlot = true;
                 for (String s : itemLore) {
                     if (s.contains("7Enchantments") && !(s.contains("and"))) {
                         s = StringUtils.stripControlCodes(s);
                         s = s.replace("Enchantments", "").replace(" ", "");
-                        currEnch = Integer.parseInt(s.substring(0, s.indexOf("/") + 1).replace("/", ""));
-                        maxEnch = Integer.parseInt(s.substring(s.indexOf("/"), s.length()).replace("/", ""));
-                        if (currEnch >= maxEnch) { 
-                            currEnch = maxEnch;
-                            g = 255F;
-                            r = b = 0F;
-                        } else {
-                            float tempFloat = (currEnch / maxEnch);
-                            if (tempFloat == 0.5F) {
-                                r = g = 255F;
-                                b = 0F;
-                            } else if (tempFloat > .5F) {
-                                r = (tempFloat * 255F);
-                                g = 255F;
-                                b = 0F;
-                            } else {
-                                g = 255 - (tempFloat * 255F);
-                                r = 255F;
-                                b = 0F;
-                            }
-                        }
+                        currEnch = Float.parseFloat(s.substring(0, s.indexOf("/") + 1).replace("/", ""));
+                        maxEnch = Float.parseFloat(s.substring(s.indexOf("/"), s.length()).replace("/", ""));
+                        compareFloats(currEnch, maxEnch);
                     }
                 }
             }
             if (dName.contains("aUltimate Enchantments")) {
                 colorThisSlot = true;
-                itemLore = is.getTooltip(mgmtp, false);
+                itemLore = is.getTooltip(mcgmtp, false);
                 for (String s : itemLore) {
                     s = StringUtils.stripControlCodes(s);
                     if (s.contains("Ultimate Enchantments")) {
@@ -101,7 +84,7 @@ public class HexatorumUtils {
             }
             if (dName.contains("aReforges")) {
                 colorThisSlot = true;
-                itemLore = is.getTooltip(mgmtp, false);
+                itemLore = is.getTooltip(mcgmtp, false);
                 for (String s : itemLore) {
                     s = StringUtils.stripControlCodes(s);
                     if (s.contains("Reforge")) {
@@ -115,51 +98,58 @@ public class HexatorumUtils {
                     }
                 }
             }
-            // if (dName.contains("aModifiers")) {
-            //     colorThisSlot = true;
-            //     itemLore = is.getTooltip(mgmtp, false);
-            //     for (String s : itemLore) {
-            //         s = StringUtils.stripControlCodes(s);
-            //         if (s.contains("Recombobulator ")) {
-            //             if (s.contains("✔")) floats.add(1F);
-            //             else floats.add(0F);
-            //         }
-            //         if (s.contains("Transmission Tuner")) {
-            //             if (s.contains("4/3") || s.contains("3/3")) floats.add(1F);
-            //             else {
-            //                 String[] sa = s.split(" ");
-            //                 s = sa[sa.length - 1];
-            //                 floats.add((float)(Integer.parseInt(s.substring(s.indexOf("/"), s.length()).replace("/", "")) / Integer.parseInt(s.substring(s.indexOf("/"), s.length()).replace("/", ""))));
-            //             }
-            //         }
-            //     }
-            // }
-            // if (dName.contains("aItem Upgrades")) {
-            //     colorThisSlot = true;
-            //     itemLore = is.getTooltip(mgmtp, false);
-            //     for (String s : itemLore) {
-            //         s = StringUtils.stripControlCodes(s);
-            //         if (s.contains("6Recombobulator ")) {
-            //             if (s.contains("✔")) {
-            //                 g = 255F;
-            //                 r = b = 0F;
-            //             } else {
-            //                 r = 255F;
-            //                 g = b = 0F;
-            //             }
-            //         }
-            //         if (s.contains("cMaster Star")) {
-                        
-            //         }
-            //     }
-            // }
+            if (dName.contains("aModifiers")) {
+                colorThisSlot = true;
+                itemLore = is.getTooltip(mcgmtp, false);
+                for (String s : itemLore) {
+                    s = StringUtils.stripControlCodes(s);
+                    if (s.contains("✔")) {
+                        floats.add(1F);
+                    } else if (s.contains("✖")) {
+                        floats.add(0F);
+                    }
+                    if (s.contains("Transmission Tuner")) {
+                        if (s.contains("4/3") || s.contains("3/3")) floats.add(1F);
+                        else {
+                            String[] sa = s.split(" ");
+                            s = sa[sa.length - 1];
+                            floats.add((Float.parseFloat(s.substring(s.indexOf("/"), s.length()).replace("/", "")) / Float.parseFloat(s.substring(s.indexOf("/"), s.length()).replace("/", ""))));
+                        }
+                    }
+                }
+            }
+            if (dName.contains("aItem Upgrades")) {
+                colorThisSlot = true;
+                itemLore = is.getTooltip(mcgmtp, false);
+                for (String s : itemLore) {
+                    s = StringUtils.stripControlCodes(s);
+                    if (s.contains("✔")) {
+                        floats.add(1F);
+                    } else if (s.contains("✖")) {
+                        floats.add(0F);
+                    }
+                    if (s.contains("Upgrade Level")) {
+                        s = s.replaceAll("  ","").replace("§7Upgrade Level", "");
+                        float currStars = 0F, maxStars = 10F;
+                        if (s.contains("➎")) currStars = 10F;
+                        else if (s.contains("➍")) currStars = 9F;
+                        else if (s.contains("➌")) currStars = 8F;
+                        else if (s.contains("➋")) currStars = 7F;
+                        else if (s.contains("➊")) currStars = 6F;
+                        String[] sa = s.split("§");
+                        if (sa.length < 1) return;
+                        else for (String str : sa) if (str.endsWith("✪")) currStars++;
+                        floats.add(currStars / maxStars);
+                    }
+                }
+            }
             if (dName.contains("Gemstones")) {
                 colorThisSlot = true;
-                itemLore = is.getTooltip(mgmtp, false);
+                itemLore = is.getTooltip(mcgmtp, false);
                 String[] sa = {};
                 for (String s : itemLore) if (s.contains("7Gemstones")) sa = s.split(" ");
                 if (sa.length < 1) return;
-                int filledGemstones = 0, availableGemstones = 0;
+                float filledGemstones = 0, availableGemstones = 0F;
                 for (String sTwo : sa) {
                     if (sTwo.contains("[") && sTwo.contains("]")) {
                         availableGemstones++;
@@ -168,29 +158,18 @@ public class HexatorumUtils {
                         }
                     }
                 }
-                if (filledGemstones >= availableGemstones) {
-                    filledGemstones = availableGemstones;
-                    g = 255F;
-                    r = b = 0F;
-                } else {
-                    ChatLib.chat(filledGemstones + "/" + availableGemstones);
-                    float tempFloat = filledGemstones / availableGemstones;
-                    if (tempFloat == 0.5F) {
-                        r = g = 255F;
-                        b = 0F;
-                    } else if (tempFloat > 0.5F) {
-                        r = (tempFloat * 255F);
-                        g = 255F;
-                        b = 0F;
-                    } else {
-                        g = 255 - (tempFloat * 255F);
-                        r = 255F;
-                        b = 0F;
-                    }
-                }
+                compareFloats(filledGemstones, availableGemstones);
             }
             if (!colorThisSlot) continue;
+            if (!(floats.isEmpty())) {
+                for (float f : floats) {
+                    if (f >= 1F) f = 1F;
+                    floatForArrayList += f;
+                }
+                compareFloats(floatForArrayList, ((float)(floats.size())));
+            }
             Color bgColor = new Color(((int)(r)), ((int)(g)), ((int)(b)));
+            ChatLib.chat(bgColor.toString() + " was selected for item named " + dName);
             GL11.glTranslated(0, 0, 1);
             Gui.drawRect(
                 ((aMagicNumber) + sl.xDisplayPosition),
@@ -205,6 +184,30 @@ public class HexatorumUtils {
                     ((anotherMagicNumber) + sl.yDisplayPosition)) + 16),
                 bgColor.getRGB());
             GL11.glTranslated(0, 0, -1);
+        }
+    }
+
+    private void compareFloats(float f1, float f2) {
+        if (f1 >= f2) {
+            f1 = f2;
+            g = 255F;
+            r = b = 0F;
+        } else {
+            float tempFloat = (f1 / f2);
+            if (tempFloat == 0.5F) {
+                r = g = 255F;
+                b = 0F;
+            } else {
+                if (tempFloat > 0.5F) {
+                    r = 255 - (tempFloat * 255F);
+                    g = 255F;
+                    b = 0F;
+                } else {
+                    r = 255F;
+                    g = (tempFloat * 255F);
+                    b = 0F;
+                }
+            }
         }
     }
 }
